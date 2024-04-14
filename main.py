937d363363cdb6a3626d70d9c1c403d35ad4b1dc
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2024-04-15 02:09:36 +0700

    add test def

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 
 def train_model(hparams):
     
-    if hparams.mode == 'real':
+    if hparams.data == 'real':
         data_dir = 'augmented'
     else:
         data_dir = 'data'
@@ -38,13 +38,14 @@ def train_model(hparams):
     val_split = pt.val_split
     test_split = pt.test_split
 
-    dataModule = ImageDataModule(data_dir=data_dir, 
+    data_module = ImageDataModule(data_dir=data_dir, 
                                  batch_size=batch_size, 
                                  num_workers=num_workers, 
                                  transform=transform, 
                                  val_split=val_split, 
                                  test_split=test_split, 
                                  random_state=random_state)
+    
     model = MobileNetV2Lightning(num_classes=num_classes, lr=lr, last_drop=last_drop)
     early_stopping = EarlyStopping(monitor='val_loss', patience=patience)
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
@@ -56,17 +57,20 @@ def train_model(hparams):
                       enable_checkpointing=True,
                       callbacks=[lr_monitor, early_stopping])
     
-    trainer.fit(model, datamodule=dataModule)
+    trainer.fit(model, datamodule=data_module)
 
-# def test_model():
-#     model = MobileNetV2Lightning.load_from_checkpoint(
-#         checkpoint_path="/path/to/pytorch_checkpoint.ckpt",
-#         hparams_file="/path/to/experiment/version/hparams.yaml",
-#         map_location=None,
-#     )
+def test_model():
+    model = MobileNetV2Lightning.load_from_checkpoint(
+        checkpoint_path="lightning_logs/version_1/checkpoints/epoch=13-step=28.ckpt",
+        hparams_file="lightning_logs/version_1/hparams.yaml",
+        map_location=None,
+    )
 
-#     trainer = Trainer(...)
-#     trainer.test(model)
+    data_module = None # Load the data module here
+
+    trainer = Trainer()
+    
+    trainer.test(model, datamodule=data_module)
 
 # def predict():
 
@@ -78,6 +82,15 @@ def train_model(hparams):
 
 #     return pred
 
+def main(args):
+    if args.mode == 'train':
+        train_model(args)
+    elif args.mode == 'test':
+        test_model()
+    else:
+        raise ValueError(f"Invalid mode: {args.mode}")
+    
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--batch_size", default=64)
@@ -85,9 +98,10 @@ if __name__ == '__main__':
     parser.add_argument("--last_drop", default=0.2)
     parser.add_argument("--max_epochs", default=20)
     parser.add_argument("--patience", default=5)
-    parser.add_argument("--mode", default=None)
+    parser.add_argument("--data", default=None)
+    parser.add_argument("--mode", default='train')
     args = parser.parse_args()
-    train_model(args)
+    main(args)
 
 
 
